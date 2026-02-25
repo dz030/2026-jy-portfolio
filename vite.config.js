72595a0c853c3d16246c5a47350ff85b129d812b
Minney Jeeyoung Min <jeeyoungmin@Minneys-MacBook-Pro.local>
2026-02-25 18:00:56 -0500

    Enable full multi-page build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from "vite";
 import htmlInject from "vite-plugin-html-inject";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/",
-  plugins: [htmlInject()]
+  plugins: [htmlInject()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+
+        aiMotionGraphic: resolve(__dirname, "projects/ai-driven/ai-motion-graphic.html"),
+        influencerTool: resolve(__dirname, "projects/ai-driven/influencer-tool.html"),
+        intelligentFulfillment: resolve(__dirname, "projects/ai-driven/intelligent-fulfillment-system.html"),
+
+        fashionWebUI: resolve(__dirname, "projects/digital-visual/fashion-web-ui-design.html"),
+        webBanners: resolve(__dirname, "projects/digital-visual/web-banners-newsletters.html"),
+
+        b2bOrderApp: resolve(__dirname, "projects/product-ux/b2b-order-app.html"),
+        b2bWebsite: resolve(__dirname, "projects/product-ux/b2b-website-redesign.html"),
+        dtcWebsite: resolve(__dirname, "projects/product-ux/dtc-website-redesign.html"),
+        nearbiewApp: resolve(__dirname, "projects/product-ux/nearbiew-app.html"),
+        shopifyPOS: resolve(__dirname, "projects/product-ux/shopify-pos-migration.html"),
+      }
+    }
+  }
 });
